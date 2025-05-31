4640ca50bdbaad9475fa252fc4b86d0cc193d968
junhyeonpark <j0303p@gmail.com>
2025-05-31 13:42:44 +0900

    first commit

---
+++ src/main/java/org/example/popspace/PopspaceApplication.java
@@ -0,0 +1,13 @@
+package org.example.popspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopspaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/popspace/PopspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.popspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
