0a2dcd3a03364610c8eda6bb3576a751e991390d
junhyeonpark <j0303p@gmail.com>
2025-06-02 09:00:42 +0900

    feat: controlleradvice를 통한 글로벌 에러처리

---
+++ src/main/java/org/example/popspace/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package org.example.popspace.global.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ErrorDTO> handleCustomException(CustomException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
+        log.warn("CustomException occurred: {}", errorCode);
+        return handleExceptionInternal(errorCode);
+    }
+
+    @ExceptionHandler(IndexOutOfBoundsException.class)
+    protected ResponseEntity<ErrorDTO> handleIndexOutOfBounds(IndexOutOfBoundsException ex) {
+        log.warn("IndexOutOfBoundsException: {}", ex.getMessage());
+        return handleExceptionInternal(ErrorCode.INVALID_INDEX);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<ErrorDTO> handleIllegalArgument(IllegalArgumentException ex) {
+        log.warn("IllegalArgumentException: {}", ex.getMessage());
+        return handleExceptionInternal(ErrorCode.INVALID_INPUT);
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorDTO> handleUncaughtException(Exception ex) {
+        log.error("Unhandled Exception", ex);
+        return handleExceptionInternal(ErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<ErrorDTO> handleExceptionInternal(ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(makeErrorResponse(errorCode));
+    }
+
+    private ErrorDTO makeErrorResponse(ErrorCode errorCode) {
+        return ErrorDTO.builder()
+                .code(errorCode.name())
+                .message(errorCode.getMessage())
+                .build();
+    }
+}
