cdc7d5bd8907d56e036ba2bdd3b7a492b39f18a4
jhy <65846779+jhy11@users.noreply.github.com>
2025-06-09 08:55:08 +0900

    refactor: convert static QR utility to @Component-managed singleton bean

--- src/main/java/org/example/popspace/util/qr/QrCodeGenerator.java
+++ src/main/java/org/example/popspace/util/qr/QrCodeGenerator.java
@@ -1,15 +1,16 @@
 package org.example.popspace.util.qr;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.common.BitMatrix;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
-
+@Component
 public class QrCodeGenerator {
 
-    public static byte[] generateQrImage(String text) throws Exception {
+    public byte[] generateQrImage(String text) throws Exception {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 300, 300);
         try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
