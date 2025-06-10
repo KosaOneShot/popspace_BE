c983b5b8f3833c033d9e300a12a02c5900db5202
junhyeonpark <j0303p@gmail.com>
2025-06-10 10:38:22 +0900

    feat: 통계 계산 로직

--- src/main/java/org/example/popspace/service/statics/StatisticsService.java
+++ src/main/java/org/example/popspace/service/statics/StatisticsService.java
@@ -36,4 +36,57 @@ public class StatisticsService {
         return calculateStatistics(entranceMembers,reservationStats,popupInfoWithLikeCount);
     }
 
+    private PopupStatisticsResponse calculateStatistics(List<ReservationMemberData> entranceMembers,
+                                                        List<ReservationTypeStateCount> reservationStats,
+                                                        PopupInfoWithLikeCount popupInfoWithLikeCount) {
+        long reviewCount = StatisticsUtils.countReview(entranceMembers);
+        log.info("리뷰 개수 {}", reviewCount);
+
+        double averageRating = StatisticsUtils.averageRating(entranceMembers, reviewCount);
+        log.info("리뷰 평점 {}", averageRating);
+
+        long totalReservationCount = reservationStats.size();
+        log.info("총 예약자 수 {}", totalReservationCount);
+
+        long entranceCount = entranceMembers.size();
+        log.info("입장 인원 수 {}", entranceCount);
+
+        long advanceTotal = StatisticsUtils.countAdvance(reservationStats);
+        log.info("사전 예약 수 {}", advanceTotal);
+
+        GenderRatio genderRatio = StatisticsUtils.createGenderRatio(entranceMembers, entranceCount);
+        log.info("입장 인원 성비{}", genderRatio);
+
+        NoShowWithCancelRatio advanceNoShowWithCancelRatio = StatisticsUtils.createAdvanceNoShowWithCancel(reservationStats, advanceTotal, "ADVANCE");
+        log.info("사전예약 노쇼, 캔슬 비율 {}", advanceNoShowWithCancelRatio);
+
+        NoShowRatio walkInNoShowRatio = StatisticsUtils.createWalkInNoShow(reservationStats, entranceCount - advanceTotal, "WALK_IN");
+        log.info("웨이팅 노쇼 비율 {}", walkInNoShowRatio);
+
+        AgeRatio ageRatio = StatisticsUtils.toAgeRatio(entranceMembers, entranceCount);
+        log.info("입장 인원 나이 비율{}", ageRatio);
+
+        List<HourlyVisitor> hourlyVisitors = StatisticsUtils.
+                analyzeHourly(entranceMembers, popupInfoWithLikeCount.getOpenTime(), popupInfoWithLikeCount.getCloseTime());
+        log.info("시간대별 입장인원 {}", hourlyVisitors);
+
+        List<WeeklyVisitor> weeklyVisitors = StatisticsUtils.
+                analyzeWeekly(entranceMembers, popupInfoWithLikeCount.getStartDate(), popupInfoWithLikeCount.getEndDate());
+        log.info("일별 입장인원 주간별 {}", weeklyVisitors);
+
+        return PopupStatisticsResponse.builder()
+                .popupName(popupInfoWithLikeCount.getPopupName())
+                .totalReservationCount(totalReservationCount)
+                .totalEntranceCount(entranceCount)
+                .likeCount(popupInfoWithLikeCount.getLikeCount())
+                .reviewCount(reviewCount)
+                .averageRating(averageRating)
+                .genderRatio(genderRatio)
+                .ageRatio(ageRatio)
+                .advanceNoShowRatio(advanceNoShowWithCancelRatio)
+                .walkInNoShowRatio(walkInNoShowRatio)
+                .weeklyVisitors(weeklyVisitors)
+                .hourlyVisitors(hourlyVisitors)
+                .build();
+    }
 }
