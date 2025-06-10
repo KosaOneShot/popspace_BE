30c5368d7623aa2d6f3dd683ad135bb6d3232c93
junhyeonpark <j0303p@gmail.com>
2025-06-10 10:40:02 +0900

    feat: 통계 계산 로직 util

---
+++ src/main/java/org/example/popspace/util/statics/StatisticsUtils.java
@@ -0,0 +1,26 @@
+package org.example.popspace.util.statics;
+
+import org.example.popspace.dto.statistics.*;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class StatisticsUtils {
+
+    // Constants
+    public static final String TYPE_ADVANCE = "ADVANCE";
+    public static final String TYPE_WALK_IN = "WALK_IN";
+    public static final String STATE_NOSHOW = "NOSHOW";
+    public static final String STATE_CANCELED = "CANCELED";
+    public static final String SEX_MALE = "M";
+
+
+
+}
