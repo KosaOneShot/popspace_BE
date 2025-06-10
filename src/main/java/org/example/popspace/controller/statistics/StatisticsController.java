488ccc50a1b18fac493f751c646ddb3a3669bb64
junhyeonpark <j0303p@gmail.com>
2025-06-10 10:35:39 +0900

    feat: popupid를 통한 통계 제공 컨트롤러

---
+++ src/main/java/org/example/popspace/controller/statistics/StatisticsController.java
@@ -0,0 +1,32 @@
+package org.example.popspace.controller.statistics;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.popspace.dto.statistics.PopupStatisticsResponse;
+import org.example.popspace.service.statics.StatisticsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+@RequiredArgsConstructor
+public class StatisticsController {
+
+    private final StatisticsService statisticsService;
+
+    //이후 활성화
+//    @PreAuthorize("hasAnyRole('ROLE_POPUP_ADMIN')")
+    @GetMapping("/popup/statistics/{popupId}")
+    public ResponseEntity<PopupStatisticsResponse> getStatisticsData(@PathVariable Long popupId) {
+
+        log.info("getStatisticsData");
+        PopupStatisticsResponse popupStatisticsResponse = statisticsService.findPopupStatistics(popupId);
+
+        return ResponseEntity.ok(popupStatisticsResponse);
+    }
+}
