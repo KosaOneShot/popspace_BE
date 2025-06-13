583dc4136d6270a904ef938dcfa3a60a8b656993
hyeesw <hyeeun7904@gmail.com>
2025-06-13 15:48:29 +0900

    refactor : 코드 리팩토링

--- src/main/java/org/example/popspace/service/reservation/ReservationService.java
+++ src/main/java/org/example/popspace/service/reservation/ReservationService.java
@@ -4,9 +4,9 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.popspace.dto.reservation.ReservationDetailResponseDto;
+import org.example.popspace.dto.reservation.ReservationListRequestDto;
 import org.example.popspace.dto.reservation.ReservationListResponseDto;
 import org.example.popspace.mapper.ReservationMapper_hyeesw;
-import java.time.LocalDate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,9 +16,9 @@ public class ReservationService {
     private final ReservationMapper_hyeesw reservationMapper;
 
     /* 예약 목록 조회 */
-    public List<ReservationListResponseDto> findReservationListByMemberId(String searchKeyword, LocalDate searchDate,
-                                                                          String reservationType, Long memberId) {
-        return reservationMapper.findReservationListByMemberId(searchKeyword, searchDate, reservationType, memberId);
+    public List<ReservationListResponseDto> findReservationListByMemberId(ReservationListRequestDto dto, Long memberId) {
+        return reservationMapper.findReservationListByMemberId(
+            dto.getSearchKeyword(), dto.getSearchDate(), dto.getReservationType(), memberId);
     }
 
     /* 예약 상세 조회 */
