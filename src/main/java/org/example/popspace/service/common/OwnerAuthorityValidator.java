4673322bb49d565c02727fef5b52335cfc38e05b
jhy <65846779+jhy11@users.noreply.github.com>
2025-06-16 11:14:08 +0900

    refactor: change userId to memberId

--- src/main/java/org/example/popspace/service/common/OwnerAuthorityValidator.java
+++ src/main/java/org/example/popspace/service/common/OwnerAuthorityValidator.java
@@ -18,21 +18,21 @@ public class OwnerAuthorityValidator {
     private final PopupMapper popupMapper;
 
     // api 요청자가 해당 예약의 팝업 사장인지 판단
-    public void validatePopupOwnerByReservation(long userId, long reserveId) {
+    public void validatePopupOwnerByReservation(long memberId, long reserveId) {
         long popupOwnerId = reservationMapper.findPopupOwnerIdByReserveId(reserveId)
                 .orElseThrow(() -> new CustomException(ErrorCode.RESERVATION_NOT_FOUND));
 
-        if (userId != popupOwnerId) {
+        if (memberId != popupOwnerId) {
             throw new CustomException(ErrorCode.NO_PERMISSION);
         }
     }
 
     // api 요청자가 해당 팝업의 사장인지 판단
-    public void validatePopupOwnerByPopup(long userId, long popupId){
+    public void validatePopupOwnerByPopup(long memberId, long popupId){
         long popupOwnerId = popupMapper.findPopupOwnerIdByPopupId(popupId)
                 .orElseThrow(() -> new CustomException(ErrorCode.RESERVATION_NOT_FOUND));
 
-        if (userId != popupOwnerId) {
+        if (memberId != popupOwnerId) {
             throw new CustomException(ErrorCode.NO_PERMISSION);
         }
     }
