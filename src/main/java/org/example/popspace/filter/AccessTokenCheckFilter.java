a2aef55ebb9d1a950a89448efce13e0fdbee96b8
junhyeonpark <j0303p@gmail.com>
2025-06-17 15:30:35 +0900

    feat: auth 용 공통 로직 util 생성

--- src/main/java/org/example/popspace/filter/AccessTokenCheckFilter.java
+++ src/main/java/org/example/popspace/filter/AccessTokenCheckFilter.java
@@ -6,21 +6,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.popspace.dto.auth.CustomUserDetail;
-import org.example.popspace.dto.auth.MemberLoginInfo;
-import org.example.popspace.mapper.redis.AuthRedisRepository;
-import org.example.popspace.util.auth.ErrorResponseUtil;
-import org.example.popspace.util.auth.ExtractCookie;
-import org.example.popspace.util.auth.JWTUtil;
 import org.example.popspace.global.error.CustomException;
 import org.example.popspace.global.error.ErrorCode;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.example.popspace.util.auth.AuthUtil;
+import org.example.popspace.util.auth.ErrorResponseUtil;
+import org.example.popspace.util.auth.ExtractCookie;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Map;
 
 
 @Slf4j
@@ -28,8 +22,7 @@ import java.util.Map;
 public class AccessTokenCheckFilter extends OncePerRequestFilter {
 
     private final String[] permittedUrls;
-    private final JWTUtil jwtUtil;
-    private final AuthRedisRepository authRedisRepository;
+    private final AuthUtil authUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -56,37 +49,15 @@ public class AccessTokenCheckFilter extends OncePerRequestFilter {
 
         try {
 
-            Map<String, Object> payload = jwtUtil.validateToken(token);
+            authUtil.setAuthenticationFromToken(token);
 
-            CustomUserDetail customUserDetail = extractUserDetailFromPayload(payload);
-
-            UsernamePasswordAuthenticationToken authentication =
-                    new UsernamePasswordAuthenticationToken(
-                            customUserDetail, null, customUserDetail.getAuthorities());
-
-            //컨텍스트에 인가 정보 추가
-            SecurityContextHolder.getContext()
-                    .setAuthentication(authentication);
-
-            log.info("token filter pass");
             filterChain.doFilter(request, response);
+
         } catch (CustomException e) {
             ErrorResponseUtil.sendJsonError(response, e.getErrorCode());
         }
     }
 
-    private CustomUserDetail extractUserDetailFromPayload(Map<String, Object> payload) {
-
-        log.info(" payload {}", payload);
-        MemberLoginInfo member= MemberLoginInfo.builder()
-                .email((String) payload.get("email"))
-                .nickname((String) payload.get("nickname"))
-                .role((String) payload.get("role"))
-                .memberId(((Number) payload.get("memberId")).longValue())
-                .build();
-
-        return CustomUserDetail.from(member);
-    }
     private boolean isPermittedPath(String path) {
         return Arrays.stream(permittedUrls).anyMatch(path::startsWith);
     }
