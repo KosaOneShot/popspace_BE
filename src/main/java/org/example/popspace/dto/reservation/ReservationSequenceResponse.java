6d4991101995a799586c408f9d0c313b7a849464
junhyeonpark <j0303p@gmail.com>
2025-06-18 17:05:02 +0900

    feat: 반환 response 변경

--- src/main/java/org/example/popspace/dto/reservation/ReservationSequenceResponse.java
+++ src/main/java/org/example/popspace/dto/reservation/ReservationSequenceResponse.java
@@ -13,12 +13,14 @@ public class ReservationSequenceResponse {
     private Integer sequence;
     private int averageWaitTime;
     private LocalTime entranceTime;  // 예: 14:30
+    private boolean isAllowed;
 
-    public static ReservationSequenceResponse of(Integer sequence,Integer averageWaitTime ,LocalTime entranceTime) {
+    public static ReservationSequenceResponse of(Integer sequence,Integer averageWaitTime ,LocalTime entranceTime,CountEntranceDTO countEntrance) {
         return ReservationSequenceResponse.builder()
                 .sequence(sequence)
                 .averageWaitTime(averageWaitTime)
                 .entranceTime(entranceTime)
+                .isAllowed(countEntrance.getIsAllowed())
                 .build();
     }
 }
